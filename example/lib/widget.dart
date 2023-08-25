8474e8d42dd91ad8ae7a780811ee3e2120f97e24
dataentry <raghavrajpurohit81@gmail.com>
2023-08-25 14:53:22 +0530

    timeline

---
+++ example/lib/widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+typedef NavigateWidgetBuilder = Widget Function();
+
+mixin NavigateMixin on Widget {
+  NavigateWidgetBuilder? get navigationBuilder;
+
+  Future<T?> navigate<T>(BuildContext context) {
+    if (navigationBuilder == null) {
+      return Future.value();
+    } else {
+      return Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => navigationBuilder!(),
+        ),
+      );
+    }
+  }
+}
+
+const kNavigationCardRadius = 8.0;
+
+class NavigationCard extends StatelessWidget with NavigateMixin {
+  const NavigationCard({
+    Key? key,
+    this.margin,
+    this.borderRadius =
+        const BorderRadius.all(Radius.circular(kNavigationCardRadius)),
+    this.navigationBuilder,
+    required this.child,
+  }) : super(key: key);
+
+  final EdgeInsetsGeometry? margin;
+  final BorderRadius? borderRadius;
+  final Widget child;
+  final NavigateWidgetBuilder? navigationBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      margin: margin,
+      shape: borderRadius != null
+          ? RoundedRectangleBorder(borderRadius: borderRadius!)
+          : null,
+      child: InkWell(
+        borderRadius: borderRadius,
+        onTap: () => navigate(context),
+        child: child,
+      ),
+    );
+  }
+}
+
+TitleAppBar(title) => AppBar(
+      title: Text(title),
+    );
